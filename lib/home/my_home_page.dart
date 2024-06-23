9f06167a89ce204ae174f79451de7b13717f44dc
David Solomon <justdavid247@gmail.com>
2024-06-23 14:22:01 +0100

    Flutter riverpod provider and state provider (video 3)

--- lib/home/my_home_page.dart
+++ lib/home/my_home_page.dart
@@ -1,67 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_dbestech/data_provider/data_provider.dart';
-import 'package:riverpod_dbestech/models/user_models.dart';
-import 'package:riverpod_dbestech/screens/detail_screen.dart';
-
-import '../services/services.dart';
 
 class MyHomePage extends ConsumerWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context, ref) {
-    final _data = ref.watch(userDataProvider);
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Riverpod'),
-      ),
-      body: _data.when(
-        data: (_data) {
-          List<UserModel> userList = _data.map((e) => e).toList();
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: Column(
-              children: [
-                Expanded(child: ListView.builder(
-                  itemCount: userList.length,
-                  itemBuilder: (_, index){
-                    return InkWell(
-                      onTap: () => Navigator.of(context).push( //Pass the data of the clicked row into the next page
-                        MaterialPageRoute(
-                          builder: (context) => DetailScreen(
-                            e: userList[index],
-                          ),
-                        )
-                      ) ,
-                      child: Card(
-                        color: Colors.blue,
-                        elevation: 4,
-                        margin: const EdgeInsets.symmetric(vertical: 10),
-                        child: ListTile(
-                          title: Text(userList[index].firstname, style: const TextStyle(
-                            color: Colors.white,
-                          ),),
-                          subtitle: Text(userList[index].lastname, style: const TextStyle(
-                            color: Colors.white
-                          ),),
-                          trailing: CircleAvatar(
-                            backgroundImage: NetworkImage(userList[index].avatar),
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ))
-              ],
-            ),
-          );
-        },
-        error: (error, e) => Text(error.toString()),
-        loading: () => const Center(
-          child: CircularProgressIndicator(),
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Provider vs StateProvider'),
         ),
+        body: SelectedButton(),
       ),
     );
   }
+}
+
+//You can access other providers insdie it and also makes changes to it but must be done internally not externally
+final isRedProvider = Provider<bool>((ref) {
+  final color = ref.watch(selectedButtonProvider);
+  return color == 'red';
+});
+
+final selectedButtonProvider = StateProvider<String>((ref) => '');
+
+class SelectedButton extends ConsumerWidget{
+  const SelectedButton({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isRed = ref.watch(isRedProvider);
+    final selectedButton = ref.watch(selectedButtonProvider);
+
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(selectedButton),
+          SizedBox(height: 10,),
+          ElevatedButton(
+            onPressed: () => ref.read(selectedButtonProvider.notifier).state = 'red', //We can change the state of StateProvider from the outside
+            child: Text('Red'),
+           ),
+
+          ElevatedButton(
+            onPressed: () => ref.read(selectedButtonProvider.notifier).state = 'blue',
+            child: Text('Blue'),
+          ),
+          SizedBox(height: 10,),
+          isRed ? Text('Color is red') : Text('Color is blue'),
+          //selectedButton == 'red' ? Text('Color is red') : Text('Color is blue'),
+        ]
+      ),
+    );
+  }
+
 }
