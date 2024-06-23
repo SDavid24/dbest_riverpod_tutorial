22dbd7f560a10601b13ccfbf83278a2b39cfc007
David Solomon <justdavid247@gmail.com>
2024-06-23 16:26:26 +0100

    Flutter Riverpod CRUD Operation _ StateNotifier & StateNotifierProvider (video 4) - Learn the difference between StateNotifier and StateProvider - Learnt how StateNotifier and StateNotifierProvider complement themselves

--- lib/home/my_home_page.dart
+++ lib/home/my_home_page.dart
@@ -1,70 +1,78 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final numbersProvider =
+StateNotifierProvider<NumberNotifier, List<String>>((ref){
+  return NumberNotifier();
+});
+
+
+class NumberNotifier extends StateNotifier<List<String>> {
+  NumberNotifier() : super(['number 12', 'number 38']);
+
+  void add(String number){
+    state = [...state, number];
+  }
+  
+  void remove(String number){
+    state = [...state.where((element) => element != number)];
+  }
+
+  void update(String number, String updatedNumber){
+    final updatedList = <String>[];
+    for (var i = 0; i < state.length; i++){
+      if(state[i] == number){
+        updatedList.add(updatedNumber);
+      }else{
+        updatedList.add(state[i]);
+      }
+    }
+    state = updatedList;
+  }
+}
 
 class MyHomePage extends ConsumerWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context, ref) {
+    final numbers = ref.watch(numbersProvider);
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Provider vs StateProvider'),
+          title: const Text('StateNotifier & StateNotifierProvider'),
         ),
-        body: SelectedButton(),
-      ),
-    );
-  }
-}
-
-//You can access other providers insdie it and also makes changes to it but must be done internally not externally
-final isRedProvider = Provider<bool>((ref) {
-  final color = ref.watch(selectedButtonProvider);
-  return color == 'red';
-});
-
-List<String> colorPatterns = [];
-final colorPatternsProvider = StateProvider<List<String>>((ref) {
-  final color = ref.watch(selectedButtonProvider);
-  colorPatterns.add(color);
-  return colorPatterns;
-});
-
-final selectedButtonProvider = StateProvider<String>((ref) => '');
-
-class SelectedButton extends ConsumerWidget{
-  const SelectedButton({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final isRed = ref.watch(isRedProvider);
-    final selectedButton = ref.watch(selectedButtonProvider);
-    final colorList = ref.watch(colorPatternsProvider);
-
-    print('colorList is: $colorList');
-    return Center(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(selectedButton),
-          SizedBox(height: 10,),
-          ElevatedButton(
-            onPressed: () => ref.read(selectedButtonProvider.notifier).state = 'red', //We can change the state of StateProvider from the outside
-            child: Text('Red'),
-           ),
-
-          ElevatedButton(
-            onPressed: () => ref.read(selectedButtonProvider.notifier).state = 'blue',
-            child: Text('Blue'),
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: FloatingActionButton(
+            onPressed: (){
+              ref
+                  .read(numbersProvider.notifier)
+                  .add('number ${Random().nextInt(100)}');
+            },
+            child: const Icon((Icons.add)),
           ),
-          SizedBox(height: 10,),
-          isRed ? Text('Color is red') : Text('Color is blue'),
-          selectedButton == 'red' ? Text('Color is red') : Text('Color is blue'),
-        ]
+        ),
+        body: Center(
+          child: Column(
+            children: numbers.map((e) => GestureDetector(
+              onLongPress: (){
+                ref.read(numbersProvider.notifier).update(e, '${e } ${Random().nextInt(100)}');
+              },
+              onTap: (){
+                ref.read(numbersProvider.notifier).remove(e);
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(e),
+              ),
+            )).toList(),
+          ),
+        ),
       ),
     );
   }
-
 }
