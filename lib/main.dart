e000e00aa200157129811f55a57c36d7546660ee
David Solomon <justdavid247@gmail.com>
2024-07-04 08:35:30 +0100

    _Generate Better Code in Seconds with Riverpod and Flutter!_ (video 14) - How to generate code in riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_dbestech/pages/frame/welcome/welcome.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+part 'main.g.dart';
+
+
+@riverpod
+String StringLabel(StringLabelRef ref) => 'Hello world of Riverpod and Bloc';
+
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
 
       ),
-      home: const WelcomePage(),
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            ref.watch(stringLabelProvider.select((value) => value)),
+            style: const TextStyle(
+              fontSize: 30,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/main.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'main.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$stringLabelHash() => r'3f10ea1034a496f8f989a7a7d52be105867d4dd6';
+
+/// See also [StringLabel].
+@ProviderFor(StringLabel)
+final stringLabelProvider = AutoDisposeProvider<String>.internal(
+  StringLabel,
+  name: r'stringLabelProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$stringLabelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef StringLabelRef = AutoDisposeProviderRef<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
